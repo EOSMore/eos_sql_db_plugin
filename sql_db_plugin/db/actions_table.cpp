f474d13c9f01c0fe7f62439af65c2e536b1839ae
shrimp <liu.shrimp@gmail.com>
2018-08-02 20:37:37 +0800

    fix filter

--- sql_db_plugin/db/actions_table.cpp
+++ sql_db_plugin/db/actions_table.cpp
@@ -60,18 +60,6 @@ namespace eosio {
         //only transfer and system action
         if(action.name.to_string() == "transfer" || action.account.to_string() == "eosio") {
 
-            if (action.name.to_string() == "transfer"
-                    && action.name.to_string() != "buyrambytes"
-                    && action.name.to_string() != "buyram"
-                    && action.name.to_string() != "sellram"
-                    && action.name.to_string() != "delegatebw"
-                    && action.name.to_string() != "undelegatebw"
-                    && action.name.to_string() != "refund"
-                    && action.name.to_string() != "newaccount"
-                    && action.name.to_string() != "setabi") {
-                return;
-            }
-
             if (action.name.to_string() == "onblock")
                 return; //system contract abi haven't onblock, so we could get abi_data.
 
